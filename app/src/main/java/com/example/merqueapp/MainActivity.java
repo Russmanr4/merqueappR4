cc967fa0e987bb81dde0dc4e72a5b51c01e0cdc7
russmanr4 <russman.riverar4@gmail.com>
2022-11-16 22:29:38 -0500

    app codigo de boton de login con cuenta de google

--- app/src/main/java/com/example/merqueapp/MainActivity.java
+++ app/src/main/java/com/example/merqueapp/MainActivity.java
@@ -11,11 +11,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText mTextInputEditTextPassword;
     Button mButtonLogin;
     FirebaseAuth mAuth;
-
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int Request_code_Google=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
         mButtonLogin = findViewById(R.id.btnlogin);
 
         mAuth= FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +68,41 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void signInGoogle (){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, Request_code_Google);
+
+    }
+
+
+    private void firebaseAuthWithGoogle(String idToken){
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+
+                            String id= mAuth.getCurrentUser().getUid();
+                            checkUserExist(id);
+
+                        }else{
+                            Log.w("error", "signInWithCredential:failure", task.getException());
+
+                        }
+
+                    }
+                });
+
+
+    }
+
+    private void checkUserExist(String id) {
+
+
+    }
+
 
     private void login() {
 
